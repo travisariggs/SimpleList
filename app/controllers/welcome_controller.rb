c1625378dac34cc23401081a72eb9288eff6388a
Travis Riggs <triggs@lexmark.com>
2015-03-06 14:27:49 -0500

    * Added a set of flash messages and alerts to the project and layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    # flash[:success] = "Test success!"
+    # flash[:info] = "Test Info!"
+    # flash[:warning] = "Test warning!"
+    # flash[:error] = "Test danger!"
   end
 
 end
