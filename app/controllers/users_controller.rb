66f9cc45cf6cf35294ba21a2aa7878b10510ea90
Travis Riggs <triggs@lexmark.com>
2015-03-08 16:09:05 -0400

    * Created new and create actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
 
+	before_action :current_user
+
   def show
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	# Set the user role to 'user'
+  	@user.role = 'user'
+
+  	if @user.save
+  		flash[:success] = "You've registered successfully!"
+  		# Log the user in
+  		session[:user_id] = @user.id
+  		redirect_to user_path(@user.id)
+  	else
+  		flash[:error].now = "Oh snap! Something went wrong with your registration."
+  		render :new
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password)
   end
 
 end
