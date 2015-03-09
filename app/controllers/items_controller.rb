38509804230df6eb852b496ab0948bba8a5648c0
Travis Riggs <triggs@lexmark.com>
2015-03-09 13:59:06 -0400

    * Fleshed out actions to handle new item and item edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,17 +3,45 @@ class ItemsController < ApplicationController
 	def index
 	end
 
+	def show
+	end
+
 	def new
 		@item = Item.new
 	end
 
 	def create
-	end
+		@item = Item.new(item_params)
 
-	def show
+		@user = current_user
+		@item.user_id = @user.id
+
+		if @item.save
+			flash[:success] = "Your item was added to the list"
+			redirect_to user_items_path(current_user)
+		else
+			flash[:error] = "Oh snap! We couldn't save your item."
+			render :new
+		end
 	end
 
 	def edit
+	end
+
+	def update
+		if @item.update(item_params)
+			flash[:info] = "Your item was updated"
+			redirect_to user_items(current_user)
+		else
+			flash[:error] = "Shucks...we couldn't update your item."
+			render :edit
+		end
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit([:description, :priority, :due_date])
 	end
 
 end
