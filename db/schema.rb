f7a0f9714a8dc3fea95ce2d96faceb9cf91f786c
Travis Riggs <triggs@lexmark.com>
2015-03-08 21:42:26 -0400

    * Added user_id foreign key to items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309013245) do
+ActiveRecord::Schema.define(version: 20150309013848) do
 
   create_table "items", force: true do |t|
     t.string   "description"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150309013245) do
     t.date     "due_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
