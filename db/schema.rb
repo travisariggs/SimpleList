3f821e4093a7bb9378bee5a0056e32873a8f6b6b
Travis Riggs <triggs@lexmark.com>
2015-03-08 21:35:36 -0400

    * Created items table to track user list items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306201107) do
+ActiveRecord::Schema.define(version: 20150309013245) do
+
+  create_table "items", force: true do |t|
+    t.string   "description"
+    t.integer  "priority"
+    t.date     "due_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
