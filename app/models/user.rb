1d6db08cb7ed6b24d6bdbcc3f0c1bb89162e0875
Travis Riggs <triggs@lexmark.com>
2015-03-10 21:54:40 -0400

    * Added a validation to ensure that user names are unique * Overriding the user model's to_param function to create URL slugging for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,15 @@ class User < ActiveRecord::Base
 
   validates :name, on: :create, length: {minimum: 2}
   validates :password, on: :create, length: {minimum: 5}
+  validates_uniqueness_of :name
+
+  def to_param
+  	# This enables URL slugging because _path methods call the 
+  	#  object model's 'to_param' method to get an :id for the URL.
+  	#  I'm overriding that method and returning the name instead.
+  	#  This ensures uniqueness because of the 'validates_uniqueness_of :name'
+  	#  above.
+  	self.name
+  end
 
 end
